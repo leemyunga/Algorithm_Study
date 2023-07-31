5d635f4d3dbe3de4c4022b89baaaad03d067ad67
LEE Myung-A <104979981+leemyunga@users.noreply.github.com>
2023-07-31 14:27:44 +0900

    Docs : Delete directory

--- lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] solution(int[] numbers) {
-        int[] answer = numbers;
-        for (int i = 0; i < answer.length; i++) {
-			answer[i] *= 2;
-		}
-        return answer;
-    }
-}
--- lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(String str1, String str2) {
-        int answer = 2;
-	        if (str1.contains(str2)) {
-	        	answer = 1;
-	        }
-	        return answer;
-    }
-}
